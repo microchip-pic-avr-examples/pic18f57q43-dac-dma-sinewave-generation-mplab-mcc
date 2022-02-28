b7f430817974d8a6e246d6ed5b6165ea1819dab6
Irene Mary Abraham <Irene.Abraham@microchip.com>
2022-02-28 12:33:22 +0530

    MPAE-13496: Added MPLAB-X project and updated readme and metadata

---
+++ pic18f57q43-dac-dma-sinewave-generation-mplab-mcc.X/mcc_generated_files/dma/src/dma1.c
@@ -0,0 +1,172 @@
+/**
+ * DMA1 Generated Driver File
+ * 
+ * @file dma1.c
+ * 
+ * @ingroup  dma1
+ * 
+ * @brief This is the generated driver implementation file for the DMA1 driver.
+ *
+ * @version DMA1 Driver Version 2.11.0
+ */ 
+
+/*
+© [2022] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+ /**
+   Section: Included Files
+ */
+
+#include <xc.h>
+#include "../dma1.h"
+
+
+/**
+  Section: DMA1 APIs
+*/
+
+void DMA1_Initialize(void)
+{
+    
+    //DMA Instance Selection : 0x0
+    DMASELECT = 0x0;
+    //Source Address : 0x010000
+    DMAnSSA = 0x010000;
+    //Destination Address : DAC1DATL
+    DMAnDSA = DAC1DATL;
+    //SSTP not cleared; SMODE incremented; SMR Program Flash; DSTP not cleared; DMODE unchanged; 
+    DMAnCON1 = 0xA;
+    //Source Message Size : 250
+    DMAnSSZ = 250;
+    //Destination Message Size : 1
+    DMAnDSZ = 1;
+    //Start Trigger : SIRQ TMR2; 
+    DMAnSIRQ = 0x1B;
+    //Abort Trigger : AIRQ None; 
+    DMAnAIRQ = 0x0;
+	
+    // Clear Destination Count Interrupt Flag bit
+    PIR2bits.DMA1DCNTIF = 0; 
+    // Clear Source Count Interrupt Flag bit
+    PIR2bits.DMA1SCNTIF = 0; 
+    // Clear Abort Interrupt Flag bit
+    PIR2bits.DMA1AIF = 0; 
+    // Clear Overrun Interrupt Flag bit
+    PIR2bits.DMA1ORIF =0; 
+    
+    PIE2bits.DMA1DCNTIE = 0;
+    PIE2bits.DMA1SCNTIE = 0;
+    PIE2bits.DMA1AIE = 0;
+    PIE2bits.DMA1ORIE = 0;
+	
+    //AIRQEN disabled; DGO not in progress; SIRQEN enabled; EN enabled; 
+    DMAnCON0 = 0xC0;
+	 
+}
+
+void DMA1_SelectSourceRegion(uint8_t region)
+{
+    DMASELECT = 0x0;
+	DMAnCON1bits.SMR  = region;
+}
+
+void DMA1_SetSourceAddress(uint24_t address)
+{
+    DMASELECT = 0x0;
+	DMAnSSA = address;
+}
+
+void DMA1_SetDestinationAddress(uint16_t address)
+{
+    DMASELECT = 0x0;
+	DMAnDSA = address;
+}
+
+void DMA1_SetSourceSize(uint16_t size)
+{
+    DMASELECT = 0x0;
+	DMAnSSZ= size;
+}
+
+void DMA1_SetDestinationSize(uint16_t size)
+{                     
+    DMASELECT = 0x0;
+	DMAnDSZ= size;
+}
+
+uint24_t DMA1_GetSourcePointer(void)
+{
+    DMASELECT = 0x0;
+	return DMAnSPTR;
+}
+
+uint16_t DMA1_GetDestinationPointer(void)
+{
+    DMASELECT = 0x0;
+	return DMAnDPTR;
+}
+
+void DMA1_SetStartTrigger(uint8_t sirq)
+{
+    DMASELECT = 0x0;
+	DMAnSIRQ = sirq;
+}
+
+void DMA1_SetAbortTrigger(uint8_t airq)
+{
+    DMASELECT = 0x0;
+	DMAnAIRQ = airq;
+}
+
+void DMA1_StartTransfer(void)
+{
+    DMASELECT = 0x0;
+	DMAnCON0bits.DGO = 1;
+}
+
+void DMA1_StartTransferWithTrigger(void)
+{
+    DMASELECT = 0x0;
+	DMAnCON0bits.SIRQEN = 1;
+}
+
+void DMA1_StopTransfer(void)
+{
+    DMASELECT = 0x0;
+	DMAnCON0bits.SIRQEN = 0; 
+	DMAnCON0bits.DGO = 0;
+}
+
+void DMA1_SetDMAPriority(uint8_t priority)
+{
+    // This function is dependant on the PR1WAY CONFIG bit
+	PRLOCK = 0x55;
+	PRLOCK = 0xAA;
+	PRLOCKbits.PRLOCKED = 0;
+	DMA1PR = priority;
+	PRLOCK = 0x55;
+	PRLOCK = 0xAA;
+	PRLOCKbits.PRLOCKED = 1;
+}
+
+/**
+ End of File
+*/
---
+++ pic18f57q43-dac-dma-sinewave-generation-mplab-mcc.X/mcc_generated_files/system/pins.h
@@ -0,0 +1,92 @@
+/**
+ * Generated Pins header File
+ * 
+ * @file pins.h
+ * 
+ * @defgroup  pinsdriver Pins Driver
+ * 
+ * @brief This is generated driver header for pins. 
+ *        This header file provides APIs for all pins selected in the GUI.
+ *
+ * @version Driver Version  3.0.0
+*/
+
+/*
+© [2022] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef PINS_H
+#define PINS_H
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set RA2 aliases
+#define IO_RA2_TRIS                 TRISAbits.TRISA2
+#define IO_RA2_LAT                  LATAbits.LATA2
+#define IO_RA2_PORT                 PORTAbits.RA2
+#define IO_RA2_WPU                  WPUAbits.WPUA2
+#define IO_RA2_OD                   ODCONAbits.ODCA2
+#define IO_RA2_ANS                  ANSELAbits.ANSELA2
+#define IO_RA2_SetHigh()            do { LATAbits.LATA2 = 1; } while(0)
+#define IO_RA2_SetLow()             do { LATAbits.LATA2 = 0; } while(0)
+#define IO_RA2_Toggle()             do { LATAbits.LATA2 = ~LATAbits.LATA2; } while(0)
+#define IO_RA2_GetValue()           PORTAbits.RA2
+#define IO_RA2_SetDigitalInput()    do { TRISAbits.TRISA2 = 1; } while(0)
+#define IO_RA2_SetDigitalOutput()   do { TRISAbits.TRISA2 = 0; } while(0)
+#define IO_RA2_SetPullup()          do { WPUAbits.WPUA2 = 1; } while(0)
+#define IO_RA2_ResetPullup()        do { WPUAbits.WPUA2 = 0; } while(0)
+#define IO_RA2_SetPushPull()        do { ODCONAbits.ODCA2 = 0; } while(0)
+#define IO_RA2_SetOpenDrain()       do { ODCONAbits.ODCA2 = 1; } while(0)
+#define IO_RA2_SetAnalogMode()      do { ANSELAbits.ANSELA2 = 1; } while(0)
+#define IO_RA2_SetDigitalMode()     do { ANSELAbits.ANSELA2 = 0; } while(0)
+
+/**
+ * @ingroup  pinsdriver
+ * @brief GPIO and peripheral I/O initialization
+ * @param none
+ * @return none
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @ingroup  pinsdriver
+ * @brief Interrupt on Change Handling routine
+ * @param none
+ * @return none
+ */
+void PIN_MANAGER_IOC(void);
+
+
+#endif // PINS_H
+/**
+ End of File
+*/
